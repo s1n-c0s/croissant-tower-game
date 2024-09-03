c43364b9f9317bdd1278af0e0ec19eeb6ef49b64
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2024-09-04 02:32:43 +0700

    + Folders

---
+++ Assets/01-Script/spawnC.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnC : MonoBehaviour
+{
+    [SerializeField] private GameObject crosisant_prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Lean.Pool.LeanPool.Spawn(crosisant_prefab, transform);
+        }
+    }
+}
