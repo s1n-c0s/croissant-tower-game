cf5aac30f61097bc821fa18cc15da84d24aca963
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2024-09-07 13:34:14 +0700

    = update

--- Assets/01-Script/TableDel.cs
+++ Assets/01-Script/TableDel.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class TableDel : MonoBehaviour
 {
+    [SerializeField] float lifetime = 1f;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.gameObject.CompareTag("Croissant"))
         {
-            Lean.Pool.LeanPool.Despawn(other.gameObject);
+            Lean.Pool.LeanPool.Despawn(other.gameObject, lifetime);
         }
     }
 }
